269be4e8221229aaa9ee6c8e58fb1daf91451b2c
El Hadji Lamine Biaye <biaye89@gmail.com>
2017-06-30 16:31:04 +0000

    Android 4.4 | Api 19

--- Xelalma/app/src/main/java/dev/mjangale/MainActivityFragment.java
+++ Xelalma/app/src/main/java/dev/mjangale/MainActivityFragment.java
@@ -28,17 +28,6 @@ import static android.content.Context.LAYOUT_INFLATER_SERVICE;
  */
 public class MainActivityFragment extends Fragment {
 
-    String TAG = "appsPage";
-
-    private PopupWindow mPopupWindow;
-    private LinearLayout mLinearLayout;
-    private View popUpView;
-    LayoutInflater popUpInflater;
-
-    FirebaseRecyclerAdapter mAdapter;
-    RecyclerView mAppGridView;
-
-    //private static final String TAG ="MainActivityFragment" ;
 
     public MainActivityFragment() {
     }
@@ -49,37 +38,7 @@ public class MainActivityFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
 
 
-        // Set up Layoutinflater for the popup windows
-        popUpInflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
 
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_main, container, false);
-
-        //TextView headerTitle = (TextView) v.findViewById(R.id.titleOfFragment);
-        //headerTitle.setText(R.string.appTitle);
-
-        mLinearLayout = (LinearLayout) v.findViewById(R.id.advPage_layout);
-
-        // Setup firebase to get information about apps
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("classes");
-
-        mAppGridView = (RecyclerView) v.findViewById(R.id.appDrawer);
-        mAppGridView.setHasFixedSize(false);
-        GridLayoutManager gm = new GridLayoutManager(getContext(), 3);
-        mAppGridView.setLayoutManager(gm);
-        mAdapter = new FirebaseRecyclerAdapter<Advices, AppViewHolder>(Advices.class, R.layout.app_icon, AppViewHolder.class, mDatabase) {
-
-            @Override
-            protected void populateViewHolder(AppViewHolder viewHolder, final Advices adv, int position) {
-
-                Glide.with(getContext()).load(adv.getLogo()).into(viewHolder.logo);
-                viewHolder.setAppName(adv.getClasse());
-
-
-            }
-        };
-
-        mAppGridView.setAdapter(mAdapter);
         
         return fragmentView;
     }
